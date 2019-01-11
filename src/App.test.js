974915d9648366565e55d4d01f68778649882512
Sam Broughton <sambroughton@sams-mbp.home>
2019-01-11 14:34:42 +0000

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashin!g', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
